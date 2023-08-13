2a7c784469372bb03e87c3829fd07569276a52b4
edwintantawi <edwintantawi.workspace@gmail.com>
2023-08-13 18:15:47 +0700

    feat: add favicon & title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from 'next';
+
 import { fontMono, fontSans } from '~/lib/fonts';
 import { cn } from '~/lib/utils';
 
 import '~/app/style.css';
+
+export const metadata: Metadata = {
+  title: 'GDSC Mikroskil',
+};
 
 export default function RootLayout({
   children,
