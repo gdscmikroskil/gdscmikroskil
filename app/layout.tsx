88d4a403401ba41a861fa5bd94e1f7d9e472516d
edwintantawi <edwintantawi.workspace@gmail.com>
2023-08-13 18:05:17 +0700

    feat: add custom font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={cn(fontSans.variable, fontMono.variable)}>
+      <body className={cn('font-sans', fontSans.variable, fontMono.variable)}>
         {children}
       </body>
     </html>
