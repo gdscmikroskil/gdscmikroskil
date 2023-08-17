214b637ade4b4612eebc7ad4d7a3a7415bdba4e9
edwintantawi <edwintantawi.workspace@gmail.com>
2023-08-17 22:45:04 +0700

    refactor: link card ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import { AlertCircleIcon, ChevronRight } from 'lucide-react';
+import { AlertCircleIcon } from 'lucide-react';
 
 import { Card } from '~/components/card';
+import { Icons } from '~/components/icons';
 import { Alert, AlertDescription, AlertTitle } from '~/components/ui/alert';
 import { env } from '~/lib/env.mjs';
 import { getErrorDetail } from '~/lib/errors';
@@ -13,7 +14,7 @@ export default function RootPage({ searchParams }: RootPageProps) {
   const error = getErrorDetail(searchParams.error);
 
   return (
-    <main>
+    <main className="mb-12">
       {error !== null && (
         <Alert variant="destructive" className="mb-4">
           <AlertCircleIcon className="h-4 w-4" />
@@ -22,22 +23,24 @@ export default function RootPage({ searchParams }: RootPageProps) {
         </Alert>
       )}
 
-      <ul className="space-y-3">
-        <li>
-          <Card
-            title="Discord Server (Azure AD)"
+      <Card.Root>
+        <Card.Group
+          title="Discord Server"
+          icon={<Icons.Discord className="fill-primary" />}
+        >
+          <Card.Link
+            title="Join as Mikroskil student"
+            subtitle="Get an exclusive role for Mikroskil students!"
             href="/api/connect"
-            icon={<ChevronRight />}
           />
-        </li>
-        <li>
-          <Card
-            title="Discord Server (Invite Link)"
+
+          <Card.Link
+            title="Join as another university student"
+            subtitle="Wherever you come from, you can still join!"
             href={env.DISCORD_INVITE_LINK}
-            icon={<ChevronRight />}
           />
-        </li>
-      </ul>
+        </Card.Group>
+      </Card.Root>
     </main>
   );
 }
