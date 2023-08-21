a5386844e100afd0a912267beaddb0a5f3e090f0
edwintantawi <edwintantawi.workspace@gmail.com>
2023-08-21 15:31:51 +0700

    feat: add title text balancer

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import Image from 'next/image';
 
+import Balancer from 'react-wrap-balancer';
+
 import gdscLogo from '~/assets/gdsc-logo.png';
 
 export function Header() {
@@ -9,7 +11,7 @@ export function Header() {
       <Image src={gdscLogo} alt="" className="mb-2 h-8 w-auto md:h-10" />
       <div className="flex flex-col justify-center">
         <h1 className="text-xl font-bold md:text-2xl">
-          Google Developer Student Clubs
+          <Balancer>Google Developer Student Clubs</Balancer>
         </h1>
         <p className="text-base text-muted-foreground md:text-lg">
           Universitas Mikroskil
