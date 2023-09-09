e4614cf7575664281b1e2ec20bf2c2207288c2bb
edwintantawi <edwintantawi.dev@gmail.com>
2023-09-09 18:41:13 +0700

    feat: make footer show in bottom screen when contents small

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from '~/components/footer';
 import { Header } from '~/components/header';
 
 export default function MainLayout({
@@ -6,9 +7,12 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <div className="flex h-full flex-col justify-stretch">
       <Header />
-      <div className="mx-auto max-w-xl px-3">{children}</div>
-    </>
+      <div className="flex-1">
+        <div className="mx-auto max-w-xl px-3">{children}</div>
+      </div>
+      <Footer />
+    </div>
   );
 }
