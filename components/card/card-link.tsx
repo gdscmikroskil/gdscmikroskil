2dddb30183296853633bc67d4546d4a9dc4ab510
edwintantawi <edwintantawi.dev@gmail.com>
2023-09-14 09:29:27 +0700

    feat: add target blank to links

--- components/card/card-link.tsx
+++ components/card/card-link.tsx
@@ -12,7 +12,11 @@ export function CardLink({ title, subtitle, href }: CardLinkProps) {
     <li>
       <article className="relative flex min-h-[60px] flex-col justify-center rounded-lg border border-border bg-slate-50 px-3 py-2 hover:border-primary hover:bg-slate-100 dark:bg-slate-900 dark:hover:bg-slate-900/50 md:min-h-[72px] md:px-4 md:py-3">
         <h3 className="text-sm md:text-base">
-          <Link href={href} className="after:absolute after:inset-0">
+          <Link
+            href={href}
+            target="_blank"
+            className="after:absolute after:inset-0"
+          >
             {title}
           </Link>
         </h3>
