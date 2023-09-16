34f92876df90b0cc467e3fde2f6dbafe7b238190
edwintantawi <edwintantawi.dev@gmail.com>
2023-09-16 23:32:25 +0700

    feat: generate qrcode

--- components/qrcode-generator.tsx
+++ components/qrcode-generator.tsx
@@ -2,6 +2,9 @@
 
 import * as React from 'react';
 
+import { QRCodeCanvas } from 'qrcode.react';
+
+import gdsclogo from '~/assets/qrcode-gdsc-logo.jpg';
 import { Icons } from '~/components/icons';
 import { Button } from '~/components/ui/button';
 import {
@@ -24,24 +27,38 @@ interface QRCodeGeneratorProps {
 }
 
 export function QRCodeGenerator({ links }: QRCodeGeneratorProps) {
-  const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState('');
+  const [open, setOpen] = React.useState<boolean>(false);
+  const [value, setValue] = React.useState<string | null>(null);
+
+  const qrCodeContainerRef = React.useRef<HTMLDivElement>(null);
+  const downloadLinkRef = React.useRef<HTMLAnchorElement>(null);
+
+  const selectedLink = links.find((link) => link._id === value);
 
   const handleSelect = (currentValue: string) => {
-    setValue(currentValue === value ? '' : currentValue);
+    setValue(currentValue === value ? null : currentValue);
     setOpen(false);
   };
 
-  const handleFilter = (value: string, search: string) => {
-    const item = links.find((link) => link._id === value);
+  const handleFilter = (filterValue: string, search: string) => {
+    const item = links.find((link) => link._id === filterValue);
     if (item?.title.toLowerCase().includes(search.toLowerCase())) {
       return 1;
     }
     return 0;
   };
 
+  const handleDownload = () => {
+    const canvasElement = qrCodeContainerRef.current?.querySelector('canvas');
+    const qrCodeDataUrl = canvasElement?.toDataURL('image/png');
+    if (downloadLinkRef.current !== null && qrCodeDataUrl !== undefined) {
+      downloadLinkRef.current.href = qrCodeDataUrl;
+      downloadLinkRef.current.click();
+    }
+  };
+
   return (
-    <div>
+    <div className="space-y-4">
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
           <Button
@@ -50,9 +67,7 @@ export function QRCodeGenerator({ links }: QRCodeGeneratorProps) {
             aria-expanded={open}
             className="w-full justify-between"
           >
-            {value
-              ? links.find((link) => link._id === value)?.title
-              : 'Select Link...'}
+            {value ? selectedLink?.title : 'Select Link...'}
             <Icons.ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
           </Button>
         </PopoverTrigger>
@@ -92,6 +107,43 @@ export function QRCodeGenerator({ links }: QRCodeGeneratorProps) {
           </div>
         </PopoverContent>
       </Popover>
+
+      <div className="aspect-square w-full rounded-md border p-4 md:p-8">
+        {selectedLink === undefined ? (
+          <div className="grid h-full place-content-center place-items-center gap-2 text-muted-foreground">
+            <Icons.Link />
+            <p className="text-xs md:text-sm">Please select the link...</p>
+          </div>
+        ) : (
+          <div ref={qrCodeContainerRef}>
+            <QRCodeCanvas
+              includeMargin
+              value={selectedLink.url}
+              size={1080}
+              bgColor="#ffffff"
+              fgColor="#000000"
+              level="H"
+              imageSettings={{
+                src: gdsclogo.src,
+                height: 256,
+                width: 256,
+                excavate: true,
+              }}
+              style={{ width: '100%', height: 'auto' }}
+            />
+          </div>
+        )}
+      </div>
+      <div>
+        <a ref={downloadLinkRef} download={selectedLink?._id} />
+        <Button
+          className="w-full"
+          onClick={handleDownload}
+          disabled={selectedLink === undefined}
+        >
+          Download
+        </Button>
+      </div>
     </div>
   );
 }
